1020b3ad8465bc5da11af78744157774343ed7b3
Srinivas Baride <srini@exidio.co>
2021-06-09 23:03:44 +0530

    Added more useful methods for identifying a session

--- types/session.go
+++ types/session.go
@@ -1,11 +1,25 @@
 package types
 
 import (
+	"fmt"
+	"reflect"
 	"sync"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
+
+func withTypePrefix(v interface{}) string {
+	t := reflect.TypeOf(v).String()
+	switch v := v.(type) {
+	case string:
+		return t + v
+	case fmt.Stringer:
+		return t + v.String()
+	default:
+		return ""
+	}
+}
 
 type Session struct {
 	ID          uint64         `json:"id,omitempty"`
@@ -18,30 +32,64 @@ type Session struct {
 }
 
 type Sessions struct {
-	m     map[string]Session
+	m     map[string]interface{}
 	mutex sync.Mutex
 }
 
 func NewSessions() *Sessions {
 	return &Sessions{
-		m: make(map[string]Session),
+		m: make(map[string]interface{}),
 	}
 }
 
-func (s *Sessions) Put(v Session) {
+func (s *Sessions) delete(v *Session) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	s.m[v.Key] = v
+	delete(s.m, withTypePrefix(v.Key))
+	delete(s.m, withTypePrefix(v.Address))
 }
 
-func (s *Sessions) Get(v string) Session {
-	return s.m[v]
-}
-
-func (s *Sessions) Delete(v string) {
+func (s *Sessions) Put(v *Session) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	delete(s.m, v)
+	s.m[withTypePrefix(v.Key)] = v
+	s.m[withTypePrefix(v.Address)] = v.Key
+}
+
+func (s *Sessions) GetForKey(k string) *Session {
+	v, ok := s.m[withTypePrefix(k)]
+	if !ok {
+		return nil
+	}
+
+	return v.(*Session)
+}
+
+func (s *Sessions) GetForAddress(k sdk.AccAddress) *Session {
+	v, ok := s.m[withTypePrefix(k)]
+	if !ok {
+		return nil
+	}
+
+	return s.GetForKey(v.(string))
+}
+
+func (s *Sessions) DeleteForKey(k string) {
+	v := s.GetForKey(k)
+	if v == nil {
+		return
+	}
+
+	s.delete(v)
+}
+
+func (s *Sessions) DeleteForAddress(k sdk.AccAddress) {
+	v := s.GetForAddress(k)
+	if v == nil {
+		return
+	}
+
+	s.delete(v)
 }
