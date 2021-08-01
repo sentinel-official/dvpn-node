82932d2d8e3c22e55d24bd2b8a2d86588d99607a
Srinivas Baride <srini@exidio.co>
2021-08-01 20:24:01 +0530

    Fixed uci commands

--- services/wireguard/wireguard_openwrt.go
+++ services/wireguard/wireguard_openwrt.go
@@ -16,30 +16,32 @@ func (w *WireGuard) Init(_ string) (err error) {
 
 func (w *WireGuard) PreUp() error {
 	commands := [][]string{
-		{"uci", fmt.Sprintf(`set network.%s="interface"`, w.cfg.IFace)},
-		{"uci", fmt.Sprintf(`uci set network.%s.proto="wireguard"`, w.cfg.IFace)},
-		{"uci", fmt.Sprintf(`uci set network.%s.private_key="%s"`, w.cfg.IFace, w.cfg.PrivateKey)},
-		{"uci", fmt.Sprintf(`uci set network.%s.listen_port="%d"`, w.cfg.IFace, w.cfg.ListenPort)},
-		{"uci", fmt.Sprintf(`uci add_list network.%s.addresses="%s"`, w.cfg.IFace, w.cfg.IPv4CIDR)},
-		{"uci", fmt.Sprintf(`uci add_list network.%s.addresses="%s"`, w.cfg.IFace, w.cfg.IPv6CIDR)},
+		{"uci", fmt.Sprintf(`set network.%s=interface`, w.cfg.IFace)},
+		{"uci", fmt.Sprintf(`set network.%s.proto=wireguard`, w.cfg.IFace)},
+		{"uci", fmt.Sprintf(`set network.%s.private_key=%s`, w.cfg.IFace, w.cfg.PrivateKey)},
+		{"uci", fmt.Sprintf(`set network.%s.listen_port=%d`, w.cfg.IFace, w.cfg.ListenPort)},
+		{"uci", fmt.Sprintf(`add_list network.%s.addresses=%s`, w.cfg.IFace, w.cfg.IPv4CIDR)},
+		{"uci", fmt.Sprintf(`add_list network.%s.addresses=%s`, w.cfg.IFace, w.cfg.IPv6CIDR)},
 
 		{"uci", fmt.Sprintf(`add firewall zone`)},
-		{"uci", fmt.Sprintf(`set firewall.@zone[-1].name="wireguard"`)},
-		{"uci", fmt.Sprintf(`set firewall.@zone[-1].input="ACCEPT"`)},
-		{"uci", fmt.Sprintf(`set firewall.@zone[-1].forward="ACCEPT"`)},
-		{"uci", fmt.Sprintf(`set firewall.@zone[-1].output="ACCEPT"`)},
-		{"uci", fmt.Sprintf(`add_list firewall.@zone[-1].network="%s"`, w.cfg.IFace)},
+		{"uci", fmt.Sprintf(`set firewall.@zone[-1].name=wireguard`)},
+		{"uci", fmt.Sprintf(`set firewall.@zone[-1].input=ACCEPT`)},
+		{"uci", fmt.Sprintf(`set firewall.@zone[-1].forward=ACCEPT`)},
+		{"uci", fmt.Sprintf(`set firewall.@zone[-1].output=ACCEPT`)},
+		{"uci", fmt.Sprintf(`add_list firewall.@zone[-1].network=%s`, w.cfg.IFace)},
 
 		{"uci", fmt.Sprintf(`add firewall forwarding`)},
-		{"uci", fmt.Sprintf(`set firewall.@forwarding[-1].src="wireguard"`)},
-		{"uci", fmt.Sprintf(`set firewall.@forwarding[-1].dest="%s"`, w.cfg.IFaceWAN)},
+		{"uci", fmt.Sprintf(`set firewall.@forwarding[-1].src=wireguard`)},
+		{"uci", fmt.Sprintf(`set firewall.@forwarding[-1].dest=%s`, w.cfg.IFaceWAN)},
 
 		{"uci", fmt.Sprintf(`add firewall forwarding`)},
-		{"uci", fmt.Sprintf(`set firewall.@forwarding[-1].src="%s"`, w.cfg.IFaceWAN)},
-		{"uci", fmt.Sprintf(`set firewall.@forwarding[-1].dest="wireguard"`)},
+		{"uci", fmt.Sprintf(`set firewall.@forwarding[-1].src=%s`, w.cfg.IFaceWAN)},
+		{"uci", fmt.Sprintf(`set firewall.@forwarding[-1].dest=wireguard`)},
 	}
 
 	for _, item := range commands {
+		fmt.Println(item[0], item[1])
+
 		cmd := exec.Command(item[0], strings.Split(item[1], " ")...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
@@ -60,6 +62,8 @@ func (w *WireGuard) Up() error {
 	}
 
 	for _, item := range commands {
+		fmt.Println(item[0], item[1])
+
 		cmd := exec.Command(item[0], strings.Split(item[1], " ")...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
