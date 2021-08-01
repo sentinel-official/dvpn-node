1c5eaa789c4390e89b53968d44369c74fbbde170
Srinivas Baride <srini@exidio.co>
2021-08-01 21:16:23 +0530

    Fixed incorrect CIDR

--- services/wireguard/types/keys.go
+++ services/wireguard/types/keys.go
@@ -2,7 +2,7 @@ package types
 
 const (
 	ConfigFileName = "wireguard.toml"
-	IPv4CIDR       = "10.8.0.2/24"
-	IPv6CIDR       = "fd86:ea04:1115::2/120"
+	IPv4CIDR       = "10.8.0.1/24"
+	IPv6CIDR       = "fd86:ea04:1115::1/120"
 	Type           = 1
 )
