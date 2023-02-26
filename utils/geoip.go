72f546a0231ec0c39424feaff9fe08aa52761fb6
Srinivas Baride <srini@exidio.co>
2023-02-26 20:12:11 +0530

    Changed Geo IP API endpoint

--- utils/geoip.go
+++ utils/geoip.go
@@ -3,39 +3,56 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/sentinel-official/dvpn-node/types"
 )
 
 func FetchGeoIPLocation() (*types.GeoIPLocation, error) {
-	resp, err := http.Get("http://ip-api.com/json")
+	var (
+		client = &http.Client{Timeout: 15 * time.Second}
+		path   = "https://ipv4.geojs.io/v1/ip/geo.json"
+	)
+
+	resp, err := client.Get(path)
 	if err != nil {
 		return nil, err
 	}
 
 	defer func() {
-		if err := resp.Body.Close(); err != nil {
+		if err = resp.Body.Close(); err != nil {
 			panic(err)
 		}
 	}()
 
-	var result struct {
-		City    string  `json:"city"`
-		Country string  `json:"country"`
-		Lat     float64 `json:"lat"`
-		Lon     float64 `json:"lon"`
-		Query   string  `json:"query"`
+	var body struct {
+		City      string `json:"city"`
+		Country   string `json:"country"`
+		IP        string `json:"ip"`
+		Latitude  string `json:"latitude"`
+		Longitude string `json:"longitude"`
 	}
 
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+	if err = json.NewDecoder(resp.Body).Decode(&body); err != nil {
+		return nil, err
+	}
+
+	latitude, err := strconv.ParseFloat(body.Latitude, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	longitude, err := strconv.ParseFloat(body.Longitude, 64)
+	if err != nil {
 		return nil, err
 	}
 
 	return &types.GeoIPLocation{
-		City:      result.City,
-		Country:   result.Country,
-		IP:        result.Query,
-		Latitude:  result.Lat,
-		Longitude: result.Lon,
+		City:      body.City,
+		Country:   body.Country,
+		IP:        body.IP,
+		Latitude:  latitude,
+		Longitude: longitude,
 	}, nil
 }
